e0c26b623c539d52b72a2976d2db92b0d95f6daf
Christian Posta <christian.posta@gmail.com>
2013-09-23 12:52:10 -0700

    added a test to force an example concurrent mod exception

--- camel-2.11.1-issues/src/test/java/posta/Camel6771Test.java
+++ camel-2.11.1-issues/src/test/java/posta/Camel6771Test.java
@@ -23,22 +23,82 @@ import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author <a href="http://www.christianposta.com/blog">Christian Posta</a>
  */
 public class Camel6771Test extends CamelTestSupport{
 
+    class DummyClass {
+        String test = "test";
+    }
+
+    @Test
+    public void testForceConcurrentModException() throws InterruptedException {
+        final HashMap<String, DummyClass> records = new HashMap<String, DummyClass>();
+        records.put("key", new DummyClass());
+
+        for (int i = 0; i < 10000; i++) {
+            records.put("key" + i, new DummyClass());
+        }
+
+        ExecutorService ser = Executors.newFixedThreadPool(1);
+        ExecutorService ser2 = Executors.newFixedThreadPool(1);
+        ser.execute(new Runnable() {
+
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(1);
+                        new HashMap<String, DummyClass>(records);
+                    } catch (InterruptedException e) {
+                        System.out.println("unexpected interrupt..");
+                    }
+                }
+            }
+        });
+
+        ser2.execute(new Runnable() {
+            private boolean bool = false;
+            @Override
+            public void run() {
+                while(true) {
+                    try {
+                        records.get("key").test = bool ? "blue" : "red";
+                        Thread.sleep(1);
+
+                        if (bool) {
+                            records.remove("key-2");
+                        } else {
+                            records.put("key-2", new DummyClass());
+                        }
+                    } catch (InterruptedException e) {
+                        System.out.println("Unexpected interrupt part 2..");
+                    }
+                }
+            }
+        });
+
+        CountDownLatch latch = new CountDownLatch(1);
+        latch.await(30, TimeUnit.SECONDS);
+    }
+
     @Test
     public void testRun() throws InterruptedException {
-
+        int multiplier = 10 * 1000;
         MockEndpoint mock = getMockEndpoint("mock:end");
 
-        mock.expectedMessageCount(2000);
+        mock.expectedMessageCount(2 * multiplier);
 
         List<String> body = null;
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 1 * multiplier; i++) {
             body = new ArrayList<String>();
 
             body.add("one" + i);
@@ -47,7 +107,7 @@ public class Camel6771Test extends CamelTestSupport{
 
         }
 
-        assertMockEndpointsSatisfied();
+        assertMockEndpointsSatisfied(3, TimeUnit.MINUTES);
     }
 
     @Override
@@ -67,9 +127,26 @@ public class Camel6771Test extends CamelTestSupport{
                         .completionPredicate(header(Exchange.SPLIT_COMPLETE))
                         .split()
                         .method(body())
+//                        .process(new Processor() {
+//                            @Override
+//                            public void process(Exchange exchange) throws Exception {
+//                                Map<?, ?> records = (Map<?, ?>) exchange.getProperty(Exchange.AGGREGATION_STRATEGY);
+//                                System.out.println("before size for exchange=: " + exchange.getIn().getHeader("breadcrumbId")
+//                                        + " size=" + records.size());
+//                            }
+//                        })
                         .to("seda:output");
 
-                from("seda:output").to("mock:end");
+                from("seda:output")
+//                        .process(new Processor() {
+//                            @Override
+//                            public void process(Exchange exchange) throws Exception {
+//                                Map<?, ?> records = (Map<?, ?>) exchange.getProperty(Exchange.AGGREGATION_STRATEGY);
+//                                System.out.println("after size for exchange=: " + exchange.getIn().getHeader("breadcrumbId")
+//                                        + " size=" + records.size());
+//                            }
+//                        })
+                        .to("mock:end");
             }
         };
     }
